75ac9f0d786ced44d865f1ab583acaaefa9e692b
Aaron Grode <agrode@amazon.com>
2023-05-10 16:51:04 -0500

    Initial commit

---
+++ src/CustomCloudWatchMetric/CustomCloudWatchMetricStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace CustomCloudWatchMetric
+{
+    public class CustomCloudWatchMetricStack : Stack
+    {
+        internal CustomCloudWatchMetricStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
